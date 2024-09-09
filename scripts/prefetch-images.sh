236e02a6d8781c64b8b3f23d69ef13c58ea6a967
tomasmatus <tomatus777@gmail.com>
2024-09-09 18:25:45 +0200

    scripts: add prefetch-images.sh

---
+++ scripts/prefetch-images.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eu
+
+kube_files=$(find . -mindepth 2 -maxdepth 2 -type f -regex '.*-kube.yaml$')
+images=$(grep "image: " ${kube_files} | awk '{ print $3 }')
+
+missing_images=()
+for image in ${images}; do
+  # if subcommand exits with error then add the image to the list
+  if ! $(podman image exists "${image}"); then
+    missing_images+=("${image}")
+  fi
+done
+
+printf "Pulling %d images:\n" ${#missing_images[@]}
+printf "%s\n" "${missing_images[@]}"
+printf "\n"
+podman pull ${missing_images[@]}
